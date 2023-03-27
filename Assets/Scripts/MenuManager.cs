f396a0ae030ec2f030161f8c5ccda83179fd1849
sergio-ic <sergiob@imagecampus.edu.ar>
2023-03-27 00:35:01 -0300

    Fix button sound effect not sounding when scene chaged

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,25 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    private AudioSource audioSource;
-
-    public GameObject hotDog;
-
-    private void Start()
-    {
-        audioSource = GetComponent<AudioSource>();
-    }
-
     public void LoadScene(string sceneName)
     {
+        ButtonsSound.instance.PlayButtonSound();
+
         SceneManager.LoadScene(sceneName);
         Time.timeScale = 1;
-        audioSource.Play();
-
-        if (sceneName == "Game") 
-        {
-            hotDog.GetComponent<HotDog>().speed = (float)2;
-        }
     }
 
     public void Exit()
