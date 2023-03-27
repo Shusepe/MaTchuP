62f4cb265e01728df386d98b3bbf3f67315bf1bf
sergio-ic <sergiob@imagecampus.edu.ar>
2023-03-27 01:08:18 -0300

    Optimize score script

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -4,25 +4,17 @@ using TMPro;
 public class Score : MonoBehaviour
 {
     public float points;
-    public TextMeshProUGUI textMeshPoints;
+    private TextMeshProUGUI textMeshPoints;
 
-    public void Start()
+    void Awake()
     {
         textMeshPoints = GetComponent<TextMeshProUGUI>();
-    }
-
-    public void Update()
-    {
         textMeshPoints.text = points.ToString("0");
     }
 
     public void PlusPoints(float entryPoints) 
     {
         points += entryPoints;
-    }
-
-    public void equalPoints(float highScore) 
-    {
-        highScore = points;
+        textMeshPoints.text = points.ToString("0");
     }
 }
