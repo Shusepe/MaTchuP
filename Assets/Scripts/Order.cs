c203f894df20929565c5ca0928b14ef95a5b5436
sergio-ic <sergiob@imagecampus.edu.ar>
2023-03-27 00:47:06 -0300

    Clean up order script

--- Assets/Scripts/Order.cs
+++ Assets/Scripts/Order.cs
@@ -1,12 +1,8 @@
-using UnityEngine;
-
 public class Order
 {
     private bool mayonnaise;
     private bool ketchup;
     private bool mustard;
-
-    private Sprite orderSprite;
 
     public Order(bool mayonnaise, bool ketchup, bool mustard) 
     {
@@ -29,32 +25,4 @@ public class Order
     {
         return mustard;
     }
-    
-    public Sprite getSprite() 
-    {
-        return orderSprite;
-    }
-
-    //public Order(bool mayonnaise, bool ketchup, bool mustard)
-    //{
-    //    this.mayonnaise = mayonnaise;
-    //    this.ketchup = ketchup;
-    //    this.mustard = mustard;
-    //    this.orderSprite = getOrderSprite(mayonnaise, ketchup, mustard);
-    //}
-
-
-    //private Sprite getOrderSprite(bool mayonnaise, bool ketchup, bool mustard)
-    //{
-    //    if (mayonnaise && ketchup && mustard)
-    //    {
-    //        return getSpriteByID(n);
-    //    }
-
-    //    if (mayonnaise && !ketchup && mustard)
-    //    {
-    //        return getSpriteByID(n + 1);
-    //    }
-    //}
-
 }
